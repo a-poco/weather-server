e05e54e1c777217a32de31b498481132f04a5bf5
Cristina Salazar <cristinasalazarm1@gmail.com>
2022-06-15 11:38:01 +0200

    add server folfer to singel repository

---
+++ index.js
@@ -0,0 +1,39 @@
+import fetch from 'node-fetch';
+import express from "express";
+import 'dotenv/config'
+// import path from "path";
+// import { fileURLToPath } from 'url';
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+
+const getTodaysDate = () => {
+  const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+  let current_datetime = new Date()
+  let formatted_date = current_datetime.getDate() + " " + months[current_datetime.getMonth()]
+  return formatted_date
+}
+
+app.get("/api", async (req, res) => {
+  const cityName = req.query.query
+  const weather = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.SECRET_KEY}&units=metric`
+  )
+  const data = await weather.json();
+
+  const response = {
+    forecast: data.weather[0].description,
+    temperature: data.main.temp,
+    name: data.name,
+    icon: data.weather[0].icon,
+    date: getTodaysDate()
+  }
+
+  res.json(response);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
